57acbced3b528d6e185b91e0cd26293de41850c3
KlaitGetsugi <valera335v@mail.ru>
2019-01-26 21:17:48 +0300

    add some fixes

---
+++ src/main/java/mvc/controllers/GetController.java
@@ -0,0 +1,9 @@
+package mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class GetController {
+
+
+}
---
+++ src/main/java/mvc/controllers/PostController.java
@@ -0,0 +1,7 @@
+package mvc.controllers;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class PostController {
+}
